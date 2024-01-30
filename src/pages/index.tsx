eb2e5106b20990bf0fba45fc0eeb95913812d886
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-01-30 10:17:47 -0300

    cria componente login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+import Login from "@/components/login";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-    
+      <Login/>
     </main>
   )
 }
